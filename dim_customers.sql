e44a5193e7b39b866cd03028ce932a8b6b5d2d71
Raksha GK <158577937+rakshakanthraj@users.noreply.github.com>
2026-02-17 14:22:22 +0530

    Delete dim_customers.sql

--- dim_customers.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='table', schema='gold') }}
-SELECT 
-    sha2(trim(cast(customer_id as string)), 256) as customer_sk,
-    * FROM silver.customers
-QUALIFY ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY customer_id) = 1
