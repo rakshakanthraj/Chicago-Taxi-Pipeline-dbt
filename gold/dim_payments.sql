38471f0c4cc957b5f54681195ceb52e1212cb8bc
Raksha GK <158577937+rakshakanthraj@users.noreply.github.com>
2026-02-18 18:50:11 +0530

    Delete gold directory

--- gold/dim_payments.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='table', schema='gold') }}
--- Note: Using silver.payments (the table you said exists in Databricks)
-SELECT 
-    sha2(trim(cast(payment_method as string)), 256) as payment_sk,
-    * FROM silver.payments
-QUALIFY ROW_NUMBER() OVER (PARTITION BY payment_method ORDER BY payment_method) = 1
--- gold/driver.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='table', schema='gold') }}
-SELECT 
-    sha2(trim(cast(location_id as string)), 256) as location_sk,
-    * FROM silver.locations
-QUALIFY ROW_NUMBER() OVER (PARTITION BY location_id ORDER BY location_id) = 1
